0a20a2d1be7a148e050790b8e6bb00d9e033a13b
Tanmoy <tanmoyie@dal.ca>
2023-03-17 02:45:00 -0300

    network diagram of supply and demand

--- data_visualization.py
+++ data_visualization.py
@@ -6,7 +6,7 @@ Outline
 
 """
 import matplotlib.pyplot as plt
-import geopandas
+from matplotlib import collections as mc
 
 
 # %% Plot customer and warehouse
@@ -33,7 +33,7 @@ def draw_initial_customer_demand(AllSt, facility, customer_df):
     facility.plot(ax=us_boundary_map, marker='p', color='red', markersize=500, alpha=1, label='Warehouse')
 
     for i in range(facility.shape[0]):
-        plt.text(x=facility.longitude[i]-1, y=facility.latitude[i]-1,
+        plt.text(x=facility.longitude[i] - 1, y=facility.latitude[i] - 1,
                  s=facility.index[i] + 1,
                  fontdict=dict(color='black', size=15))
     plt.axis('off')
@@ -41,7 +41,7 @@ def draw_initial_customer_demand(AllSt, facility, customer_df):
     # Add legend
     plt.legend(facecolor='white', title='Locations')
     # Add title
-    plt.title('Customer and warehouses locations')
+    # plt.title('Customer and warehouses locations')
     plt.show()
 
 
@@ -82,3 +82,34 @@ def draw_open_close_facility(AllSt, facility, customer_df):
     plt.yticks([])
     # Show plot
     plt.show()
+
+
+# %% Network Diagram
+def draw_network_diagram(assignment, AllSt, facility0, customer_df0):
+    # fig, ax = plt.subplots()
+    us_boundary_map = AllSt.boundary.plot(figsize=(18, 12), color='Black', linewidth=.5)
+    # customer
+    customer_df0.plot(ax=us_boundary_map, marker='o', color='blue', markersize=50, alpha=0.1, label='Customer')
+    # Plot potential facility locations as points
+    facility0.plot(ax=us_boundary_map, marker='p', color='red', markersize=500, alpha=1, label='Warehouse')
+
+    for i in range(facility0.shape[0]):
+        plt.text(x=facility0.longitude[i] - 1, y=facility0.latitude[i] - 1,
+                 s=facility0.index[i] + 1,
+                 fontdict=dict(color='black', size=15))
+
+    # plot the line segments, indicent points, and base station points of the final network
+    unique_stations = assignment['facility no.'].unique()
+    for ust in range(len(unique_stations)):
+        d1 = assignment.loc[assignment['facility no.'] == unique_stations[ust]].reset_index()
+        new_list = []
+        for r in range(d1.shape[0]):
+            new_list.append([(d1.c_longitude[r], d1.c_latitude[r]), (d1.f_longitude[r], d1.f_latitude[r])])
+        lc = mc.LineCollection(new_list, colors=f'C{ust + 1}',
+                               alpha=.2)  # alpha = (ust/len(unique_stations)), colors=ust,
+        us_boundary_map.add_collection(lc)
+
+    plt.axis('off')
+
+    plt.show()
+    plt.savefig('Outputs/network.png', transparent=True)
--- model1.py
+++ model1.py
@@ -9,31 +9,36 @@ from gurobipy import Model, GRB, quicksum
 import pandas as pd
 
 
-def solve(I, J, p, d_ij, N):
+def solve(I, J, p, d_ij, N, facility, customer_df):
     """
     :param I:
     :param J:
     :param p:
     :param d_ij:
     :param N:
+    :param facility:
+    :param customer_df:
     :return:
     """
 
     # Model
     mdl = Model('location allocation IP')
     # Decision Variables
-    z = mdl.addVars(I, vtype=GRB.BINARY, name='z')
-    x = mdl.addVars(J, vtype=GRB.BINARY, name='x')
+    z = mdl.addVars(I, J, vtype=GRB.BINARY, name='z')  # customer
+    x = mdl.addVars(J, vtype=GRB.BINARY, name='x')  # facility
 
     # Add constraints
     # Constraint1 what it is doing?
-    C_capacity = mdl.addConstrs((quicksum((d_ij[i, j] * x[j]) for j in J) >= z[i] for i in I), name='capacity')
+    # C_capacity = mdl.addConstrs((quicksum((d_ij[i, j] * x[j]) for j in J)
+    #               >= z[i, j] for i in I for j in J), name='capacity')
+    c1 = mdl.addConstrs((z[i, j] <= x[j] for i in I for j in J), name='c1')
     C_max_facility = mdl.addConstr(quicksum((x[j]) for j in J) == N, name='max_facility')
+    C_facility_per_customer = mdl.addConstrs((z.sum(i, '*') <= 1 for i in I), name='facility_per_customer')
 
     # Objective
-    obj = quicksum(p[i] * (z[i]) for i in I)
+    obj = quicksum((p[i] - d_ij[i, j]) * z[i, j] for i in I for j in J)
     mdl.setObjective(obj, sense=GRB.MAXIMIZE)
-    lpfile = 0  # mdl.write(mdl.lp)
+
     mdl.write('Outputs/model1.lp')
     # Run the model
     mdl.optimize()
@@ -51,4 +56,26 @@ def solve(I, J, p, d_ij, N):
 
     decision_df = pd.DataFrame(decision, columns=["decisions"])
 
-    return lpfile, attrfile, decision_df
+    #%% Extract sol
+    # Extract assignment variables
+    z_series = pd.Series(mdl.getAttr('X', z))
+    z_1s = z_series[z_series > 0.5]
+    x_series = pd.Series(mdl.getAttr('X', x))
+    x_1s = x_series[x_series > 0.5]
+
+    sol_z = pd.Series(mdl.getAttr('X', z))
+    sol_z.name = 'Assignments'
+    sol_z.index.names = ['customer no.', 'facility no.']
+    assignment0 = sol_z[sol_z > 0.5].to_frame()
+    assignment_name = assignment0.reset_index()
+
+    # organize data
+    customer_df2 = pd.DataFrame(customer_df[['longitude', 'latitude']]).reset_index()
+    customer_df2.columns = ['customer no.', 'c_longitude', 'c_latitude']
+    facility_df = pd.DataFrame(facility[['longitude', 'latitude']]).reset_index()
+    facility_df.columns = ['facility no.', 'f_longitude', 'f_latitude']
+    assignment2 = pd.merge(assignment_name[['customer no.', 'facility no.']],
+                           facility_df[['facility no.', 'f_longitude', 'f_latitude']], on='facility no.')
+    assignment = pd.merge(assignment2, customer_df2[['customer no.', 'c_longitude', 'c_latitude']])
+
+    return z_1s, x_1s, customer_df2, facility_df, assignment, attrfile, decision_df, assignment_name
